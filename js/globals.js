83311d4bb6ff3440621fc96aa288e92e904e10e2
Chrysxandria M <chrysxandria.m@gmail.com>
2017-08-21 22:28:42 -0400

    setup script, html, and globals scaffolding

---
+++ js/globals.js
@@ -0,0 +1,17 @@
+var g = {
+  moodColors: {
+    h: , // happy
+    s: , // sad
+    a: , // angry
+    n: , // neutral
+    o: , // :O
+  },
+
+  oliviaRel: 4,
+
+  mcFirstName: 'Riley',
+  mcLastName: 'Tennant',
+  princeFirstName: 'Liam',
+
+  currChoices: [],
+}
