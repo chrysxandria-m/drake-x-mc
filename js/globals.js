7d848bd470026b586e4420ddece1ad403cce109c
Chrysxandria M <chrysxandria.m@gmail.com>
2017-08-22 13:48:32 -0400

    spacing commit; skip

--- js/globals.js
+++ js/globals.js
@@ -1,17 +1,17 @@
 var g = {
-  moodColors: {
-    h: ['7AC3CC', '3083A3'], // happy
-    s: ['74797f', '282c33'], // sad
-    a: ['984B52', '3E080C'], // angry
-    n: ['74909A', '2E3F49'], // neutral
-    o: ['FEBD60', 'CD6F2E'], // :O
-  },
+    moodColors: {
+        h: ['7AC3CC', '3083A3'], // happy
+        s: ['74797f', '282c33'], // sad
+        a: ['984B52', '3E080C'], // angry
+        n: ['74909A', '2E3F49'], // neutral
+        o: ['FEBD60', 'CD6F2E'], // :O
+    },
 
-  oliviaRel: 4,
+    oliviaRel: 4,
 
-  mcFirstName: 'Riley',
-  mcLastName: 'Tennant',
-  princeFirstName: 'Liam',
+    mcFirstName: 'Riley',
+    mcLastName: 'Tennant',
+    princeFirstName: 'Liam',
 
-  currChoices: [],
+    currChoices: [],
 }
